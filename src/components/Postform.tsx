4fe1a58aa5ab9eb93523b182f4c588498a3f8a39
Rafał Chełkowski <rchelek@gmail.com>
2019-09-08 23:36:50 +0200

    Add controlled component input/textarea in PostForm

--- src/components/Postform.tsx
+++ src/components/Postform.tsx
@@ -1,20 +1,30 @@
-import React, {Component} from 'react'
+import React, {Component, ChangeEvent} from 'react'
 
 
 export default class PostForm extends Component {
 
+    state={
+        title: '',
+        body:'',
+    }
+        
+    onChange=(e:ChangeEvent<HTMLInputElement>|ChangeEvent<HTMLTextAreaElement>)=>{
+        this.setState({
+            [e.target.name]:e.target.value
+        })
+    }
+    
     render(){
         return(
             <div>
                 <h1>Write a Post</h1>
                 <form className='App_newPost'>
                     <label>Title:
-                        <input type='text' placeholder='Name your post'/>
+                        <input name= 'title' type='text' placeholder='Name your post' onChange={this.onChange} value={this.state.title}/>
                     </label>
                     <label>Text:
-                        <textarea name='body'/>
+                        <textarea name='body' onChange={this.onChange}/>
                     </label>
-
                 </form>
             </div>
         )
